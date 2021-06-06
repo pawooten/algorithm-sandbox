f5b92ad77935b0515694bcab5e925be46bb010a5
Paul Wooten <Paul.Wooten@hyland.com>
2021-06-06 19:47:31 -0400

    Initial commit

---
+++ AlgorithmSandbox/AlgorithmSandbox/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AlgorithmSandbox
+{
+    class Program
+    {
+        static void Main( string[] args )
+        {
+            Console.WriteLine( "Hello World!" );
+        }
+    }
+}
